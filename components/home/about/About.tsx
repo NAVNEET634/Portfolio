a66971d86bd8477884b7cb3ebc19997c63c64226
Navneet Kumar <navneetkumar92042@gmail.com>
2024-11-27 07:31:32 +0000

    Updated

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -13,45 +13,45 @@ export const About = () => {
         <div>
           <Reveal>
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
-              Hi there! I'm an experienced Full Stack Developer with a proven track
-              record in the IT sector. I specialize in building robust, scalable, and
-              efficient solutions using a variety of technologies, including React.js,
-              Node.js, React Native, JavaScript, HTML, CSS, MySQL, MongoDB, PHP, WordPress,
-              and Zoho Creator.
+            Hi there! I&apos;m an experienced Full Stack Developer with a proven track
+            record in the IT sector. I specialize in building robust, scalable, and
+            efficient solutions using a variety of technologies, including React.js,
+            Node.js, React Native, JavaScript, HTML, CSS, MySQL, MongoDB, PHP, WordPress,
+            and Zoho Creator
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              My expertise spans across the full software development lifecycle, from crafting responsive front-end designs
-              to optimizing back-end performance and database management. I ensure seamless functionality and high-quality
-              outcomes in all projects I work on.
+            My expertise spans across the full software development lifecycle, from crafting responsive front-end designs
+            to optimizing back-end performance and database management. I ensure seamless functionality and high-quality
+            outcomes in all projects I work on.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I thrive in both application development and support. Whether it’s resolving complex technical challenges,
-              providing post-deployment support, or performing performance tuning, I ensure uninterrupted functionality and
-              superior user experiences.
+            I thrive in both application development and support. Whether it&apos;s resolving complex technical challenges,
+            providing post-deployment support, or performing performance tuning, I ensure uninterrupted functionality and
+            superior user experiences.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I'm committed to staying ahead of industry trends, including machine learning and emerging technologies.
-              I continuously research and implement best practices to bring innovative and efficient solutions to the
-              teams I collaborate with.
+            I&apos;m committed to staying ahead of industry trends, including machine learning and emerging technologies.
+            I continuously research and implement best practices to bring innovative and efficient solutions to the
+            teams I collaborate with.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              My technical skill set includes advanced proficiency in PHP, WordPress, Zoho Creator, functional programming,
-              cross-browser compatibility, and web performance optimization. I excel in building accessible, user-friendly
-              websites and applications that meet diverse business requirements.
+            My technical skill set includes advanced proficiency in PHP, WordPress, Zoho Creator, functional programming,
+            cross-browser compatibility, and web performance optimization. I excel in building accessible, user-friendly
+            websites and applications that meet diverse business requirements.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              If you're looking for a versatile Full Stack Developer who excels at both development and ongoing support,
-              let's work together to turn your ideas into high-quality, maintainable digital solutions!
+            If you&apos;re looking for a versatile Full Stack Developer who excels at both development and ongoing support,
+            let&apos;s work together to turn your ideas into high-quality, maintainable digital solutions!
             </p>
           </Reveal>
           <Reveal>
